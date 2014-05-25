e4c1a38f3aa7261ca0ee4ee574dea599b6ee06e1
Winter Faulk <winter@faulk.me>
2014-05-24 23:44:08 -0700

    First Commit

---
+++ users.config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * uid = The user id, you can get this by going to config under Wallabag and generate a feed token
+ * key = Create a unique string to use as the authentication for accessing the API.
+ */
+
+
+$config[] = array( "uid" => 1, "key" => "" );
+
+?>
