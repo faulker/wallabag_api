9fd53f9d1d62b7762c11a7701c160958466bd204
Winter Faulk <winter@faulk.me>
2014-05-26 11:22:48 -0700

    Small Changes
    * Made $valid require TRUE
    * Reversed the array output so the newest items are first.
    * Other small changes

--- users.config.php
+++ users.config.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * uid = The user id, you can get this by going to config under Wallabag and generate a feed token
- * key = Create a unique string to use as the authentication for accessing the API.
+ * uid = The user id, you can get this by going to config page under Wallabag and generate a feed token.
+ * key = Create a unique string to use as the authentication for accessing the API, this should be long.
  */
 
 
